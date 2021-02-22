69ccbb230a70cbe2a0f4b05e5ed4c6aed85ac95b
nathan wilson <ntwilson@gmail.com>
2021-02-22 11:48:20 -0500

    silly spelling error
    Co-authored-by: Matthew Dupont <67288750+mjdupontMEA@users.noreply.github.com>

--- SafetyFirst.Specs/NonEmptySpec.fs
+++ SafetyFirst.Specs/NonEmptySpec.fs
@@ -7,12 +7,11 @@ open FSharpPlus
 open SafetyFirst
 
 [<Test>]
-let ``can map and append non empty colletions via FSharpPlus`` () = 
+let ``can map and append non empty collections via FSharpPlus`` () = 
   let (NonEmpty xs) = ((+) "hello ") <<| NonEmpty.assume ["world"; "SafetyFirst"]  
   test <@ xs = ["hello world"; "hello SafetyFirst"] @>
 
 
   let (NonEmpty ys) = (NonEmpty.assume [|1.0 .. 5.0|] |>> ((+) 2.0)) ++ (NonEmpty.assume [|7.0 .. 9.0|] |>> ((+) 1.0))
   test <@ ys = [| 3.0 .. 10.0|] @>
-
 
